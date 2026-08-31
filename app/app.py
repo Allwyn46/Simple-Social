90a58c9b94af878f5f74caa65a1e7dcc86ddd2f2
allwyn46 <allwyn153@gmail.com>
2026-08-31 19:18:16 +0530

    Added get_post and upload_post APIs

--- app/app.py
+++ app/app.py
@@ -1,8 +1,9 @@
-from fastapi import FastAPI,HTTPException
+from fastapi import FastAPI,HTTPException,File,UploadFile,Form,Depends
 from app.schemas import PostCreate,PostResponse,text_posts
 from app.db import Post, create_db_and_tables, get_async_session
 from sqlalchemy.ext.asyncio import AsyncSession
 from contextlib import asynccontextmanager
+from sqlalchemy import select
 
 @asynccontextmanager
 async def lifeSpan(app:FastAPI):
@@ -11,27 +12,44 @@ async def lifeSpan(app:FastAPI):
 
 app = FastAPI(lifespan=lifeSpan)
 
-@app.get("/posts")
-def get_all_posts(limit: int = None):
-    if limit:
-        return list(text_posts.values())[:limit]
-    return text_posts
 
-@app.get("/posts/{id}")
-def get_post(id:int):
-    if id not in text_posts:
-        raise HTTPException(status_code=404,detail="Post Not Found")
-    return text_posts.get(id)
+@app.get("/feed")
+async def get_posts(session:AsyncSession = Depends(get_async_session)):
+    feedData = await session.execute(select(Post).order_by(Post.created_at.desc()))
+    posts = [row[0] for row in feedData.all()]
+
+    posts_data = []
+    for post in posts:
+        posts_data.append(
+            {
+                "id": str(post.id),
+                "caption": post.caption,
+                "url": post.url,
+                "file_type": post.file_type,
+                "file_name": post.file_name,
+                "created_at": post.created_at
+            }
+        )
+
+    return {"posts": posts_data}
+
+@app.post("/upload")
+async def upload_post(
+    file:UploadFile = File(...),
+    caption:str = Form(""),
+    session:AsyncSession = Depends(get_async_session)
+):
     
-@app.post("/posts")
-def create_post(post:PostCreate)->PostResponse:
-    new_post = {"title":post.title,"content":post.content}
-    text_posts[max(text_posts.keys())+1] = new_post
-    return new_post
+    post = Post(
+        caption=caption,
+        url="dummy url",
+        file_type="photo",
+        file_name="dummy name"
+    )
 
-@app.delete("/posts/{id}")
-def delete_post(id: int):
-    if id not in text_posts:
-        raise HTTPException(status_code=404, detail="Post not found")
-    del text_posts[id]
-    return {"message": "Post deleted successfully"}
+    session.add(post)
+    await session.commit()
+    await session.refresh(post)
+    return post
+
+
