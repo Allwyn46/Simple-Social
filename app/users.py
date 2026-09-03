379250d7878dee29f8359fbea39481c0768cd87d
allwyn46 <allwyn153@gmail.com>
2026-09-03 19:33:52 +0530

    Added User manager class

--- app/users.py
+++ app/users.py
@@ -1,4 +1,5 @@
 import os
+import uuid
 from typing import Optional
 from fastapi import Depends, Request
 from fastapi_users import BaseUserManager, FastAPIUsers, UUIDIDMixin, models
@@ -8,3 +9,20 @@ from fastapi_users.authentication import (
     JWTStrategy
 )
 from fastapi_users.db import SQLAlchemyUserDatabase
+from app.db import User, get_user_db
+
+SECRET = os.getenv("USER_TABLE_SECRET")
+
+class UserManager(UUIDIDMixin, BaseUserManager[User, uuid.UUID]):
+    reset_password_token_secret = SECRET
+    verification_token_secret = SECRET 
+
+    async def on_after_register(self, user:User, request:Optional[Request] = None):
+        print(f"User {user.id} has Registered")
+
+async def get_user_manager(user_db: SQLAlchemyUserDatabase = Depends(get_user_db)):
+    yield(user_db)
+
+
+
+bearer_transport = BearerTransport(tokenUrl="auth/jwt/login")
