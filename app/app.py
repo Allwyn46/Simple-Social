6c828c22a4b00fe386fbfc926dbb43a7d1c99c6d
allwyn46 <allwyn153@gmail.com>
2026-09-03 20:16:28 +0530

    Fixed prrefix and tag in the router

--- app/app.py
+++ app/app.py
@@ -19,10 +19,10 @@ async def lifeSpan(app:FastAPI):
 app = FastAPI(lifespan=lifeSpan)
 
 app.include_router(fastapi_users.get_auth_router(auth_backend),prefix="/auth/jwt", tags=["auth"])
-app.include_router(fastapi_users.get_auth_router(UserRead,UserCreate),prefix="/auth", tags=["auth"])
+app.include_router(fastapi_users.get_register_router(UserRead,UserCreate),prefix="/auth", tags=["auth"])
 app.include_router(fastapi_users.get_reset_password_router(),prefix="/auth", tags=["auth"])
 app.include_router(fastapi_users.get_verify_router(UserRead),prefix="/auth", tags=["auth"])
-app.include_router(fastapi_users.get_users_router(UserRead, UserUpdate),prefix="/auth", tags=["auth"])
+app.include_router(fastapi_users.get_users_router(UserRead, UserUpdate),prefix="/user", tags=["user"])
 
 
 @app.get("/feed")
--- app/users.py
+++ app/users.py
@@ -13,22 +13,25 @@ from app.db import User, get_user_db
 
 SECRET = os.getenv("USER_TABLE_SECRET")
 
+
 class UserManager(UUIDIDMixin, BaseUserManager[User, uuid.UUID]):
     reset_password_token_secret = SECRET
-    verification_token_secret = SECRET 
+    verification_token_secret = SECRET
 
-    async def on_after_register(self, user:User, request:Optional[Request] = None):
+    async def on_after_register(self, user: User, request: Optional[Request] = None):
         print(f"User {user.id} has Registered")
 
-async def get_user_manager(user_db: SQLAlchemyUserDatabase = Depends(get_user_db)):
-    yield(user_db)
 
+async def get_user_manager(user_db: SQLAlchemyUserDatabase = Depends(get_user_db)):
+    yield UserManager(user_db)
 
 
 bearer_transport = BearerTransport(tokenUrl="auth/jwt/login")
 
-def get_jwt_strategy():
-    return JWTStrategy(secret=SECRET,lifetime_seconds=3600)
+
+def get_jwt_strategy() -> JWTStrategy:
+    return JWTStrategy(secret=SECRET, lifetime_seconds=3600)
+
 
 auth_backend = AuthenticationBackend(
     name="jwt",
@@ -36,5 +39,9 @@ auth_backend = AuthenticationBackend(
     get_strategy=get_jwt_strategy
 )
 
-fastapi_users = FastAPIUsers[User,uuid.UUID](get_user_manager,auth_backends=[auth_backend])
+fastapi_users = FastAPIUsers[User, uuid.UUID](
+    get_user_manager,
+    auth_backends=[auth_backend]
+)
+
 current_active_user = fastapi_users.current_user(active=True)
